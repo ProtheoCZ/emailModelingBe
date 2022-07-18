0ec9374786685653137ba124326506f43e4394f5
Tomas Jirku <tomas.jirku@plus4u.net>
2022-07-18 11:09:47 +0200

    views for returning list of graphData folder contents

--- emailModeling/urls.py
+++ emailModeling/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('getGraph/', views.get_graph)
+    path('getGraph/', views.get_graph),
+    path('getGraphList/', views.get_list_of_available_graphs)
    ]
--- emailModeling/views.py
+++ emailModeling/views.py
@@ -1,9 +1,21 @@
 import json
-from django.http import JsonResponse
+import os
+
+from django.http import JsonResponse, HttpResponse
+
+
 # Create your views here.
 
 
 def get_graph(request):
     file = open('graphData/editedGraphBigger.json')
-    retjson = json.load(file)
-    return JsonResponse(retjson)
+    ret_json = json.load(file)
+    return JsonResponse(ret_json)
+
+
+def get_list_of_available_graphs(request):
+    # graph_list = os.listdir('graphData')
+    graph_list = [file for file in os.listdir('graphData')]
+    print(graph_list)
+    ret_json = {"graphList": graph_list}
+    return JsonResponse(ret_json)
